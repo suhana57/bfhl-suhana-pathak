e74dd28af8eb72e06713a2ea86d49f2dd03bf2b6
Suhana Pathak <suhanapathak57@gmail.com>
2026-05-26 17:06:50 +0530

    add root route

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,6 @@ app.get("/", (req, res) => {
 
 app.use("/tickets", require("./routes/tickets"));
 
-mongoose.connect("your_atlas_uri").then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
     app.listen(process.env.PORT || 5000, () => console.log("running"));
 });
