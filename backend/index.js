effe50770c692980eb5a2f78a6191d46faed88b4
Suhana Pathak <suhanapathak57@gmail.com>
2026-05-26 17:04:32 +0530

    add root route

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-    app.listen(process.env.PORT || 5000, () => console.log("running"));
-});
 app.get("/", (req, res) => {
     res.json({ message: "DeskFlow API is running" });
 });
-app.use("/bfhl/tickets", require("./routes/tickets"));
+
+app.use("/tickets", require("./routes/tickets"));
+
+mongoose.connect("your_atlas_uri").then(() => {
+    app.listen(process.env.PORT || 5000, () => console.log("running"));
+});
